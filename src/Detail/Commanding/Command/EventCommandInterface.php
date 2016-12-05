a9ea43d75c8e71af240bf80e48f5ae38f036564d
Ivan Wolf <iwolf@detailag.ch>
2016-12-05 17:50:01 +0100

    Defined missing methods in interface

--- src/Detail/Commanding/Command/EventCommandInterface.php
+++ src/Detail/Commanding/Command/EventCommandInterface.php
@@ -2,6 +2,23 @@
 
 namespace Detail\Commanding\Command;
 
+use Zend\EventManager\EventManagerInterface;
+
 interface EventCommandInterface
 {
+    /**
+     * Retrieve the event manager instance.
+     *
+     * Lazy-initializes one if none present.
+     *
+     * @return EventManagerInterface
+     */
+    public function getEventManager();
+
+    /**
+     * Set the event manager instance.
+     *
+     * @param EventManagerInterface $events
+     */
+    public function setEventManager(EventManagerInterface $events);
 }
